b8f561ba827baee1427bfe84afffdc649bbfa60e
Apple27022002 <100838894+Apple27022002@users.noreply.github.com>
2022-07-05 08:45:16 +0700

    lan1

--- src/main/java/com/example/airbnb/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.airbnb.repository;
-
-import com.example.airbnb.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
